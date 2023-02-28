c294bc55cfdef1a997686f6c1d5317cca9e70f8d
hohoihung <w10546.ho@gmail.com>
2023-02-28 14:08:32 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,17 +9,24 @@ input.onButtonPressed(Button.B, function () {
 radio.setGroup(8)
 makerbit.connectSerialMp3(DigitalPin.P12, DigitalPin.P13)
 makerbit.setMp3Volume(22)
+serial.redirectToUSB()
+pins.setPull(DigitalPin.P0, PinPullMode.PullUp)
 basic.showIcon(IconNames.Pitchfork)
 basic.forever(function () {
-    if (tinkercademy.ADKeyboard(ADKeys.A, AnalogPin.P0)) {
+    if (pins.analogReadPin(AnalogPin.P0) < 25 && pins.analogReadPin(AnalogPin.P0) > 10) {
         radio.sendNumber(10)
-    } else if (tinkercademy.ADKeyboard(ADKeys.B, AnalogPin.P0)) {
+        serial.writeValue("Key A", pins.analogReadPin(AnalogPin.P0))
+    } else if (pins.analogReadPin(AnalogPin.P0) < 130 && pins.analogReadPin(AnalogPin.P0) > 120) {
         radio.sendNumber(11)
-    } else if (tinkercademy.ADKeyboard(ADKeys.C, AnalogPin.P0)) {
+        serial.writeValue("Key B", pins.analogReadPin(AnalogPin.P0))
+    } else if (pins.analogReadPin(AnalogPin.P0) < 210 && pins.analogReadPin(AnalogPin.P0) > 190) {
         radio.sendNumber(12)
-    } else if (tinkercademy.ADKeyboard(ADKeys.D, AnalogPin.P0)) {
+        serial.writeValue("Key C", pins.analogReadPin(AnalogPin.P0))
+    } else if (pins.analogReadPin(AnalogPin.P0) < 230 && pins.analogReadPin(AnalogPin.P0) > 220) {
         radio.sendNumber(13)
-    } else if (tinkercademy.ADKeyboard(ADKeys.E, AnalogPin.P0)) {
+        serial.writeValue("Key D", pins.analogReadPin(AnalogPin.P0))
+    } else if (pins.analogReadPin(AnalogPin.P0) < 300 && pins.analogReadPin(AnalogPin.P0) > 290) {
         radio.sendNumber(14)
+        serial.writeValue("Key E", pins.analogReadPin(AnalogPin.P0))
     }
 })
